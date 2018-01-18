68d2aa94cbb0960f244f5206126a26d68ee1e595
bhagath <bhagath@getcustomexposure.com>
2018-01-18 17:21:43 +0530

    implementing OnDestroy lifecycle hook

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	
   	this.user = {
   		id: this.route.snapshot.params['id'],
   		name: this.route.snapshot.params['name']
   	};
+
   	this.route.params
   	.subscribe(
   		(params: Params) => {
@@ -23,6 +27,10 @@ export class UserComponent implements OnInit {
   			this.user.name = params['name'];
   		}
   	);
+  }
+
+  ngOnDestroy() {
+  	this.paramsSubscription.Unsubscription();
   }
 
 }
